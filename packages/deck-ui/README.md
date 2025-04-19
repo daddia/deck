31f536ec812a4bbedb98de159c50559502fc80c4
daddia <jonathan@savvycollective.com.au>
2025-04-19 11:38:55 +1000

    Update deck-ui package.json and ESLint configuration, enhance build scripts, and remove unused files. Change homepage URL and improve type support with new dependencies. Refactor PostCSS configuration and update TypeScript settings for better path resolution.

---
+++ packages/deck-ui/README.md
@@ -0,0 +1,5 @@
+# deck UI
+
+deck UI (`dui`) is the component libray for **deck.** the presentation framework.
+
+[deck.](https://deck.daddia.com)
--- packages/deck-ui/eslint.config.mjs
+++ packages/deck-ui/eslint.config.mjs
@@ -1,4 +1,16 @@
-import { config } from "@repo/eslint-config/react-internal";
+import next from 'eslint-config-custom/next.js';
 
-/** @type {import("eslint").Linter.Config} */
-export default config;
+export default [
+  ...next,
+  {
+    rules: {
+      // for the import hacks
+      '@typescript-eslint/consistent-type-imports': 'off',
+      // some arrays like link items won't be changed
+      'react/no-array-index-key': 'off',
+      '@typescript-eslint/unbound-method': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'import/no-relative-packages': 'off',
+    },
+  },
+];
